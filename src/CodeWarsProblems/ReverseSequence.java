7507e321c24a3a896dcb68b1c1f0d330bc9bdf65
Iulian <iulian.ionut.buga@gmail.com>
2023-02-24 14:47:41 +0200

    New Branch

---
+++ src/CodeWarsProblems/ReverseSequence.java
@@ -0,0 +1,17 @@
+package CodeWarsProblems;
+
+
+public class ReverseSequence {
+    public static void main(String[] args) {
+
+        int z = 10;
+
+
+        for (int i = 0; i <= z; i++) {
+            --z;
+
+            System.out.println(z);
+
+        }
+    }
+}
