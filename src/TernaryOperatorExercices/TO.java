73f09cf3900b4519c348efe640e5c37847d68986
Iulian <iulian.ionut.buga@gmail.com>
2023-02-25 14:12:23 +0200

    New Branch

---
+++ src/TernaryOperatorExercices/TO.java
@@ -0,0 +1,24 @@
+package TernaryOperatorExercices;
+
+public class TO {
+    public static void main(String[] args) {
+
+        // Rewrite the following using ternary operator :
+        //if(x%2 == 0)
+        //c = ‘E’;
+        //else
+        //c = ‘O’;
+
+        int x = 0;
+        char c = 'c';
+
+        c = (x % 2 == 0) ? 'E' :'O';
+
+        //Rewrite the following statements using ternary operator.
+        //if(ch> “C”)
+        //value = 200;
+        //else
+        //value =100;
+
+    }
+}
