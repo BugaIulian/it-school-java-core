d20157c201edc57ae2a58cfaa312dfe4c2d02ca9
Iulian <iulian.ionut.buga@gmail.com>
2023-03-01 13:46:21 +0200

    Challege Update 27Feb

---
+++ src/PlayGround/EndsWith.java
@@ -0,0 +1,14 @@
+package PlayGround;
+
+public class EndsWith {
+    public static void main(String[] args) {
+
+        String str1 = "abc";
+        String str2 = "bc";
+        boolean isTrue = false;
+
+        if (str1.endsWith(str2)) {
+            isTrue = true;
+        }
+    }
+}
