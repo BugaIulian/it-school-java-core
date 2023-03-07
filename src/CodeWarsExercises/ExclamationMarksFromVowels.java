0c9686b6bce99b39e5b1270f20349b93fa539ecd
Iulian <iulian.ionut.buga@gmail.com>
2023-03-07 15:43:33 +0200

    Update second of March

--- src/CodeWarsExercises/ExclamationMarksFromVowels.java
+++ src/CodeWarsExercises/ExclamationMarksFromVowels.java
@@ -5,17 +5,16 @@ public class ExclamationMarksFromVowels {
     public static void main(String[] args) {
         System.out.println(replace(" Replace all the vowels with exclamation marks"));
     }
+
     public static String replace(final String s) {
 
         StringBuilder somethingUseful = new StringBuilder();
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            switch (c) {
-                case 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U':
-                    c = '!';
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + c);
+            if (c == 'a' || c == 'A' || c == 'e' || c == 'E' || c == 'i' || c == 'I' || c == 'o' || c == 'O' || c == 'u' || c == 'U') {
+                c = '!';
+            } else {
+                throw new IllegalStateException("Unexpected value: " + c);
             }
             somethingUseful.append(c);
         }
