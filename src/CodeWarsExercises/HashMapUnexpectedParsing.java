01338d0b8206a194fd5b59bdb9dbe6448417dfd8
Iulian <iulian.ionut.buga@gmail.com>
2023-03-13 18:24:29 +0200

    Update 12th of March, Undestanding better the OOP principle.

--- src/CodeWarsExercises/HashMapUnexpectedParsing.java
+++ src/CodeWarsExercises/HashMapUnexpectedParsing.java
@@ -2,6 +2,7 @@ package CodeWarsExercises;
 //https://www.codewars.com/kata/54fdaa4a50f167b5c000005f/train/java
 import java.util.HashMap;
 
+
 public class HashMapUnexpectedParsing {
     public static void main(String[] args) {
         getStatus(false);
